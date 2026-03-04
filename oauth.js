f7506857103b750d9bdcaf57262569323a6efb95
luthieria <121467175+luthieria@users.noreply.github.com>
2026-03-04 16:59:28 -0400

    Create oauth.js

---
+++ oauth.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({ error: "No code provided" });
+  }
+
+  const response = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: new URLSearchParams({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      }),
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
